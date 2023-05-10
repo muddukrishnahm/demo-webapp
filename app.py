4b622400147b0f7382da21952241265403dd5d92
mk <muddukrishna@gmail.com>
2023-05-11 01:50:25 +0530

    adde

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! Everyone! How are you today !"
+    return "Welcome! Everyone! Good morning!"
 
 @app.route('/how are you')
 def hello():
