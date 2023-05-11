945cf12b33b38e8eb8d4172a4555d866094dc8ea
mk <muddukrishna@gmail.com>
2023-05-11 08:55:40 +0530

    new feature

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! Everyone! Good morning!"
+    return "Welcome! Everyone! Good day to you all !"
 
 @app.route('/how are you')
 def hello():
